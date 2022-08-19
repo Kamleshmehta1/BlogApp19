b20969676cf873cc2aeab9eef4144fcce035650d
Kamleshmehta1 <kamleshm1999@gmail.com>
2022-08-19 17:20:19 +0530

    make it better changes111

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname + "/frontend/build")));
+  app.use(express.static("/frontend/build"));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname + "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
